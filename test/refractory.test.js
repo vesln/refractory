5215cdf2e8a58794263ec19f264f820515ff9941
Jake Luer <jake@alogicalparadox.com>
2013-11-10 07:20:47 -0500

    add `module` support and path slugs
    - `module` as first argument to resolve relative paths
    - slugs defined on the primary export replaced in path: `{{HOME}}`

--- test/refractory.test.js
+++ test/refractory.test.js
@@ -1,20 +1,20 @@
 var join = require('path').join;
 var refractory = require('..');
 
-var first = join(__dirname, 'fixtures', 'first');
-var second = join(__dirname, 'fixtures', 'second');
-var third = join(__dirname, 'fixtures', 'third');
+var first = './fixtures/first';
+var second = './fixtures/second';
+var third = './fixtures/third';
 
 describe('refractory', function() {
   it('tries to load files based on argument priority', function() {
-    var load = refractory(first, second, third);
+    var load = refractory(module, first, second, third);
     expect(load('a')).to.eq('first');
     expect(load('b')).to.eq('second');
     expect(load('c')).to.eq('third');
   });
 
   it('tries to load multiple variations of files based on argument priority', function() {
-    var load = refractory(first, second, third);
+    var load = refractory(module, first, second, third);
     expect(load('pre-a', 'a')).to.eq('first');
     expect(load('pre-b', 'b')).to.eq('second');
     expect(load('pre-c', 'c')).to.eq('third');
@@ -24,13 +24,18 @@ describe('refractory', function() {
   });
 
   it('tries to load files as node_modules lastly', function() {
-    var load = refractory(first);
+    var load = refractory(module, first);
     expect(load('chai')).to.be.ok; // jshint ignore: line
   });
 
+  it('replaces slugged template strings', function() {
+    refractory.HOME = join(__dirname, 'fixtures/home');
+    var load = refractory(module, '{{HOME}}/refractory');
+    expect(load('b')).to.eq('home');
+  });
 
   it('throws a sane error when it cannot load a file', function() {
-    var load = refractory(first);
+    var load = refractory(module, first);
     var err = null;
 
     try {
